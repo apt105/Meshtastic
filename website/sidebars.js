5c4e587ebbb64032959e07de046aebc7b5c18731
apt105 <73254487+apt105@users.noreply.github.com>
2021-04-11 15:14:19 +0100

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,12 +1,11 @@
 module.exports = {
   "Getting Started": [
     "getting_started/overview_getting_started",
-    
     {
       "Flashing Firmware": [
         "getting_started/flashing_firmware_getting_started",
         "getting_started/firmware/linux",
-        "getting_started/firmware/windows"
+        "getting_started/firmware/windows",
       ],
     },
     "getting_started/concepts_getting_started",
