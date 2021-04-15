3a50d9b09c499904f3e728a56ea2e935fb5f55ae
apt105 <73254487+apt105@users.noreply.github.com>
2021-04-15 02:39:51 +0100

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -57,8 +57,8 @@ module.exports = {
       ],
     },
     {
-      "software/web-app-software"
-    }
+      "software/web-app-software",
+    },
     {
       Other: [
         "software/other/sw-design",
